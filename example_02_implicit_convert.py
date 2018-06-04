f659a40df1807bfefe3778acc192ab2109f8a340
Bai-Cheng(Ryan) Jeng <rjeng@nvidia.com>
2018-06-04 17:34:01 +0800

    refactor

--- example_02_implicit_convert.py
+++ example_02_implicit_convert.py
@@ -24,20 +24,65 @@ import numpy as np
 from pdb import set_trace
 import sys
 
+def get_dataset(num_classes, img_h, img_w):
+  # the data, split between train and test sets
+  (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+  if K.image_data_format() == 'channels_first':
+    x_test = x_test.reshape(x_test.shape[0], 1, img_h, img_w)
+    input_shape = (1, img_h, img_w)
+  else:
+    x_test = x_test.reshape(x_test.shape[0], img_h, img_w, 1)
+    input_shape = (img_h, img_w, 1)
+
+  x_test = x_test.astype('float32')
+  x_test /= 255
+
+  # convert class vectors to binary class matrices
+  y_test  = keras.utils.to_categorical(y_test,  num_classes)
+  return x_test, y_test
+
+
+def show_number(img, img_h, img_w):
+  res = img.reshape((img_h, img_w))
+  str=''
+  for y in range(0, img_h):
+    for x in range(0, img_w):
+      data = res[y][x]
+      if (data > 0.5) : str += '@'
+      else : str += ' '
+    str += '\n'
+  print(str)
+
+
+def verify(pre, ans):
+  passed = 0
+  num_test = ans.shape[0]
+
+  for i in range(0, num_test):
+    a = np.argmax(ans[i])
+    p = np.argmax(pre[i])
+    if (p == a) : passed = passed + 1
+
+  if (passed / num_test > 0.99) : print('PASSED')
+  else : print('FAILURE', passed)
+
+  p = np.argmax(pre[0])
+  print('first inference result:', p, '\n\n')
+
+
 def get_tf_graph(model):
   with K.get_session() as sess:
-      #image_batch_t = tf.placeholder(tf.float32, shape=(None, 224, 224, 3))
       image_batch_t = tf.placeholder(tf.float32, shape=(None, 28, 28, 1), name='image_tensor')
       K.set_learning_phase(0)
       conf_t = model(image_batch_t)
       output_names = [conf_t.name[:-2]]
-      print('***debug***, output_names', output_names)
       graphdef = sess.graph.as_graph_def()
       frozen_graph = tf.graph_util.convert_variables_to_constants(sess, graphdef, output_names)
       frozen_graph = tf.graph_util.remove_training_nodes(frozen_graph)
 
-  print('***debug***, frozen_graph', type(frozen_graph))
   return frozen_graph, 'image_tensor', output_names
+
 
 ################################################################################
 # execute a graphdef
@@ -71,17 +116,7 @@ def run_graphdef(graph_def, input_str, output_str, input_data):
   with tf.Session(graph=g, config=config) as sess:
     val = sess.run(out, {inp: input_data})
   return val
-################################################################################
 
-def summary(model):
-  model.summary()
-  nb_layers = len(model.layers)
-  for i in range(0, nb_layers):
-    print('***debug***, layer name ', i, model.layers[i].name)
-  for i in range(0, nb_layers):
-    print('***debug***, input name ', i, model.layers[i].input.name)
-  for i in range(0, nb_layers):
-    print('***debug***, output name', i, model.layers[i].output.name)
 
 ################################################################################
 # conversion example
@@ -108,69 +143,37 @@ def convert_tftrt_fp(orig_graph, batch_size, precision, output_str):
   if precision=='FP16' or precision=='FP32':
     return trt_graph
 
+
 def main(argv):
   num_classes = 10
   # input image dimensions
-  img_rows, img_cols = 28, 28
-
-  # the data, split between train and test sets
-  (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-  if K.image_data_format() == 'channels_first':
-      x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-      x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-      input_shape = (1, img_rows, img_cols)
-  else:
-      x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-      x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-      input_shape = (img_rows, img_cols, 1)
-
-  x_train = x_train.astype('float32')
-  x_test = x_test.astype('float32')
-  x_train /= 255
-  x_test /= 255
-  print('x_train shape:', x_train.shape)
-  print(x_train.shape[0], 'train samples')
-  print(x_test.shape[0], 'test samples')
-
-  # convert class vectors to binary class matrices
-  y_train = keras.utils.to_categorical(y_train, num_classes)
-  y_test = keras.utils.to_categorical(y_test, num_classes)
+  img_h, img_w = 28, 28
 
   model = load_model("my_model.h5")
-  summary(model)
+  model.summary()
 
+  x_test, y_test = get_dataset(num_classes, img_h, img_w)
   score = model.evaluate(x_test, y_test, verbose=0)
   # make sure load right model
   print('Test loss:', score[0])
   print('Test accuracy:', score[1])
 
-  predict = model.predict(x_test)
-  # print predict to compared TRT result
-  print('***debug***, x_test.shape', x_test.shape)
-  print('***debug***, predict[0]', predict[0])
+  show_number(x_test[0], img_h, img_w)
+  y_predict = model.predict(x_test)
+  verify(y_predict, y_test)
 
   tf_graph, input_name, output_name = get_tf_graph(model)
-  print('***debug***, input_name', type(input_name), input_name)
-  print('***debug***, output_name', type(output_name), output_name)
   #print('***debug***, tf_graph', type(tf_graph), tf_graph)
-  print('***debug***, tf_graph', type(tf_graph))  
+  y_predict_tf = run_graphdef(tf_graph, input_name, output_name, x_test)
+  verify(y_predict_tf, y_test)
 
-  tf_result = run_graphdef(tf_graph, input_name, output_name, x_test)
-  print('***debug***, tf_result', type(tf_result), tf_result.shape)
-  print('***debug***, tf_result', tf_result[0])
-
-  print('***debug***, after run_graphdef')
   trt_graph = convert_tftrt_fp(tf_graph, 1, 'FP32', output_name[0]) 
   #print('***debug***, trt_graph', type(trt_graph), trt_graph)
-  print('***debug***, trt_graph', type(trt_graph))
-
   print('***debug***, after convert_tftrt_fp')
   x_1_test = x_test[0]
   x_1_test = x_1_test.reshape((1, 28, 28, 1))
-  print('***debug***, x_1_test.shape', x_1_test.shape)
-  trt_result = run_graphdef(trt_graph, input_name, output_name, x_1_test)
-  print('***debug***, trt_result', trt_result)
+  y_predict_trt = run_graphdef(trt_graph, input_name, output_name, x_1_test)
+  print('***debug***, y_predict_trt', np.argmax(y_predict_trt))
 
 
 if __name__ == "__main__":

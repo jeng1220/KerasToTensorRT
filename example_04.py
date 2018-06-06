2bcaf6fa0ca78d96be6c81caeca508fb7dbd03d5
Bai-Cheng(Ryan) Jeng <rjeng@nvidia.com>
2018-06-06 13:01:13 +0800

    add tensorflow to compare it

--- example_04.py
+++ example_04.py
@@ -5,6 +5,10 @@ from keras.models import Model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Input, merge, Concatenate, Conv2D, MaxPooling2D
 from keras import backend as K
+
+import tensorflow as tf
+from tensorflow.contrib import tensorrt as tftrt
+
 from optparse import OptionParser
 from pdb import set_trace
 import utils.ascii as helper
@@ -50,6 +54,76 @@ def preprocess_data(x, y, num_classes):
 
   return x_part_a, x_part_b, y_out
 
+def get_tf_graph(model, s):
+  with K.get_session() as sess:
+      input_a = tf.placeholder(tf.float32, shape=(None, s[0], s[1], s[2]), name='image_tensor_a')
+      input_b = tf.placeholder(tf.float32, shape=(None, s[0], s[1], s[2]), name='image_tensor_b')
+      K.set_learning_phase(0)
+      conf_t = model([input_a, input_b])
+      dst_strs = [conf_t.name[:-2]]
+      graphdef = sess.graph.as_graph_def()
+      frozen_graph = tf.graph_util.convert_variables_to_constants(sess, graphdef, dst_strs)
+      frozen_graph = tf.graph_util.remove_training_nodes(frozen_graph)
+
+  return frozen_graph, ['image_tensor_a', 'image_tensor_b'], dst_strs
+
+def get_tf_sess(graph_def, input_str, output_str):
+  # load TF-TRT graph into memory and extract input & output nodes
+  g = tf.Graph()
+  with g.as_default():
+    ina, inb, out = tf.import_graph_def(
+        graph_def=graph_def, return_elements=[input_str[0], input_str[1], output_str[0]])
+    ina = ina.outputs[0]
+    inb = inb.outputs[0]
+    out = out.outputs[0]
+  # allow_growth and restrict Tensorflow to claim all GPU memory
+  # currently TensorRT engine uses independent memory allocation outside of TF
+  config = tf.ConfigProto(gpu_options=
+             tf.GPUOptions(per_process_gpu_memory_fraction=0.5,
+             allow_growth=True))
+  # we can now import trt_graph into Tensorflow and execute it. If given target
+  sess = tf.Session(graph=g, config=config)
+  return sess, [ina, inb], out
+
+def run_tf_sess(sess, inp, out, input_data):
+  #set_trace()
+  val = sess.run(out, {inp[0]: input_data[0], inp[1]: input_data[1]})
+  #val = sess.run(out)
+  return val
+
+def tensorflow_infer(graph_def, input_str, output_str, input_data):
+  sess, inp, out = get_tf_sess(graph_def, input_str, output_str)
+  val = run_tf_sess(sess, inp, out, input_data)
+  return val
+
+class tftrt_engine():
+  def __init__(self, graph, src_str, dst_str, batch_size, precision):
+    #set_trace()
+    tftrt_graph = tftrt.create_inference_graph(
+      input_graph_def=graph, # native Tensorflow graphdef
+      outputs=dst_str,
+      max_batch_size=batch_size,
+      max_workspace_size_bytes=1 << 25,
+      precision_mode=precision,
+      minimum_segment_size=2)
+    sess, src_str, dst_str = get_tf_sess(tftrt_graph, src_str, dst_str)
+    self.sess = sess
+    self.src_str = src_str
+    self.dst_str = dst_str
+    self.batch_size = batch_size
+
+  def infer(self, x_a, x_b):
+    num_test = x_a.shape[0]
+    num_class = 100
+    y = np.empty((num_test, num_class), np.float32)
+    batch_size = self.batch_size
+    for i in range(0, num_test, self.batch_size):
+      x_part_a = x_a[i : i + batch_size, : ]
+      x_part_b = x_b[i : i + batch_size, : ]
+      y_part = run_tf_sess(self.sess, self.src_str, self.dst_str, [x_part_a, x_part_b])
+      y[i : i + batch_size, : ] = y_part
+    return y
+
 def main():
   parser = OptionParser()
   parser.add_option('-f', '--format', dest='format',
@@ -83,7 +157,7 @@ def main():
 
   output_shape, model = custom_model(input_shape, num_classes)
   batch_size = 100
-  epochs = 4
+  epochs = 1
   model.fit([x_train_a, x_train_b], y_train,
             batch_size=batch_size,
             epochs=epochs,
@@ -93,11 +167,20 @@ def main():
   print('Test loss:', score[0])
   print('Test accuracy:', score[1])
 
-  model.save(filename)
+  #model.save(filename)
 
   helper.print_ascii(x_test_a[0], img_h, img_w)
   helper.print_ascii(x_test_b[0], img_h, img_w)
-  print(np.argmax(y_test[0]))
+  y_keras = model.predict([x_test_a, x_test_b])
+  print(np.argmax(y_keras[0]))
+
+  tf_graph, src_str, dst_str = get_tf_graph(model, input_shape)
+  y_tf = tensorflow_infer(tf_graph, src_str, dst_str, [x_test_a, x_test_b])
+  print(np.argmax(y_tf[0]))
+
+  tftrt_graph = tftrt_engine(tf_graph, src_str, dst_str, 1000, 'FP32')
+  y_tftrt = tftrt_graph.infer(x_test_a, x_test_b)
+  print(np.argmax(y_tftrt[0]))
 
 if __name__ == '__main__':
   main()
